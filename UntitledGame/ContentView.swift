e14e714b95785f8688ede18573b3bebb68d399ed
Snesnopic <snesnopic@gmail.com>
2023-12-05 12:24:04 +0100

    Initial Commit

---
+++ UntitledGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UntitledGame
+//
+//  Created by Giuseppe Francione on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UntitledGame/UntitledGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  UntitledGameApp.swift
+//  UntitledGame
+//
+//  Created by Giuseppe Francione on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UntitledGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
