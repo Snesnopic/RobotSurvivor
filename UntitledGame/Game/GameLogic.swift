455eb22bee4018bbbbf855b1c562a72c1c36410d
Giuseppe Casillo <g.casillox2@gmail.com>
2023-12-07 11:35:24 +0100

    GameLogic created

--- UntitledGame/Game/GameLogic.swift
+++ UntitledGame/Game/GameLogic.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class GameLogic: ObservableObject {
+    
+    static let shared: GameLogic = GameLogic()
+    
+    func setUpGame(){
+        self.currentScore = 0
+        self.currentXP = 0
+        self.isGameOver = false;
+    }
+    
+    @Published var time: TimeInterval = 0
+    @Published var currentScore: Int = 0
+    @Published var currentXP: Int = 0
+    @Published var isGameOver = false
+    
+    func increaseScore(points: Int){
+        self.currentScore = self.currentXP + points
+    }
+    
+    func increaseTime(by t: TimeInterval){
+        self.time = self.time + t
+    }
+    
+    func finishGame(){
+        if self.isGameOver == false{
+            self.isGameOver = true
+        }
+    }
+    
+    func restartGame(){
+        self.setUpGame()
+    }
+    
+}
