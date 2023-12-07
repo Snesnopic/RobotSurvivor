18ef8ae5e59a311c652e35c886346fe4e070df49
Whisper010 <zinatullinlinar2003@gmail.com>
2023-12-07 11:00:20 +0100

    GameScene

--- UntitledGame/GameScene.swift
+++ UntitledGame/GameScene.swift
@@ -8,7 +8,17 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    // Keeps track of when the last update happend.
+    // Used to calculate how much time has passed between updates.
+    var lastUpdate: TimeInterval = 0
+    
     override func didMove(to view: SKView) {
         print("You are in the game scene!")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        
     }
 }
