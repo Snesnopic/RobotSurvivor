b0e871b9c922fd9a72379a11c5017e19ebadf2a1
Snesnopic <snesnopic@gmail.com>
2023-12-11 16:02:22 +0100

    Created another enemy type

--- UntitledGame/ViewModel/EnemyTypesVM.swift
+++ UntitledGame/ViewModel/EnemyTypesVM.swift
@@ -11,6 +11,7 @@ class EnemyTypesVM {
     var enemyTypes: [EnemyType] = [
     EnemyType(name: "Wasp", health: 3, speed: 50),
     EnemyType(name: "Scarab", health: 5, speed: 25),
-    EnemyType(name: "Spider", health: 7, speed: 13)
+    EnemyType(name: "Spider", health: 7, speed: 30),
+    EnemyType(name: "Hornet", health: 6, speed: 35)
     ]
 }
