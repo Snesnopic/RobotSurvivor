4bb7509df57bff7d9d22c664b5e3e141259108f5
Snesnopic <snesnopic@gmail.com>
2023-12-11 10:36:19 +0100

    removed playerNode as a scene, now has a reference to the joystick and calls setupgame on init

--- UntitledGame/Game/GameScene.swift
+++ UntitledGame/Game/GameScene.swift
@@ -10,25 +10,25 @@ import Foundation
 
 
 
-class playerNode: SKScene {
-    var playerNode: SKSpriteNode!
-    
-    override func didMove(to view: SKView) {
-        
-        playerNode = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-        playerNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        addChild(playerNode)
-        
-        playerNode.physicsBody = SKPhysicsBody(rectangleOf: playerNode.size)
-        playerNode.physicsBody?.affectedByGravity = false
-        playerNode.physicsBody?.allowsRotation = false
-    }
-}
+//class playerNode: SKScene {
+//    var playerNode: SKSpriteNode!
+//    
+//    override func didMove(to view: SKView) {
+//        
+//        playerNode = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+//        playerNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
+//        playerNode.physicsBody = SKPhysicsBody(rectangleOf: playerNode.size)
+//        playerNode.physicsBody?.affectedByGravity = false
+//        playerNode.physicsBody?.allowsRotation = false
+//        
+//    }
+//}
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var player: SKSpriteNode!
+    var joystick: Joystick!
     // Keeps track of when the last update happend.
     // Used to calculate how much time has passed between updates.
     var gameLogic: GameLogic = GameLogic.shared
@@ -36,6 +36,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         print("You are in the game scene!")
+        setUpGame()
     }
     
     override func update(_ currentTime: TimeInterval) {
