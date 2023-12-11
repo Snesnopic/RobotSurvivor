a232edcf71f5fbc92c50e86530559de8f2a2faa5
Snesnopic <snesnopic@gmail.com>
2023-12-11 10:35:52 +0100

    createPlayer now works, setUpGame is not private anymore, joystick initialization in progress

--- UntitledGame/Game/Components/SceneSetUp.swift
+++ UntitledGame/Game/Components/SceneSetUp.swift
@@ -10,13 +10,18 @@ import SpriteKit
 
 extension GameScene {
     
-    private func setUpGame() {
+    func setUpGame() {
         //needs gameLogic reference in GameScene
         self.gameLogic.setUpGame()
+       
         self.backgroundColor = SKColor.darkGray
         //there is no player to create yet!
-//        let playerInitialPosition = CGPoint(x: self.frame.width/2, y: self.frame.height/6)
-//        self.createPlayer(at: playerInitialPosition)
+        let playerInitialPosition = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
+        self.createPlayer(at: playerInitialPosition)
+        self.joystick = Joystick(player: player)
+        self.joystick.name = "joystick"
+        self.joystick.position = CGPoint(x: 10, y: 10)
+        addChild(joystick)
     }
     
     private func setUpPhysicsWorld() {
@@ -26,10 +31,11 @@ extension GameScene {
     
     
     private func createPlayer(at position: CGPoint) {
-        self.player = SKSpriteNode(imageNamed: "")
+        self.player = SKSpriteNode(imageNamed: "stick")
         self.player.name = "player"
         self.player.position = position
         player.physicsBody = SKPhysicsBody(circleOfRadius: 25.0)
+        self.player.physicsBody?.affectedByGravity = false
         addChild(self.player)
     }
     
