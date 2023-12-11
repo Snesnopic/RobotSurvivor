96deb636ed7ab3641c657d8b1e4594afeb675215
Whisper010 <zinatullinlinar2003@gmail.com>
2023-12-11 11:23:04 +0100

    enemy node

--- UntitledGame/Game/Components/EnemyBehavior.swift
+++ UntitledGame/Game/Components/EnemyBehavior.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SpriteKit
+
+class EnemyNode: SKSpriteNode {
+    var type: EnemyType
+    
+    init(type: EnemyType) {
+       
+        self.type = type
+        let texture = SKTexture(imageNamed: type.name)
+        super.init(texture: texture,color: .white, size: texture.size())
+        
+        name = "enemy"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("LOL NO")
+    }
+}
---
+++ UntitledGame/Game/Components/EnemyType.swift
@@ -0,0 +1,14 @@
+//
+//  EnemyType.swift
+//  UntitledGame
+//
+//  Created by Linar Zinatullin on 11/12/23.
+//
+
+import Foundation
+
+struct EnemyType {
+    let name: String
+    let health: Int
+    let speed: CGFloat
+}
