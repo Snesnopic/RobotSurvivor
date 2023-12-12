29c4918cbebdd814a9e5b2dd2ec772118dbf3425
Snesnopic <snesnopic@gmail.com>
2023-12-12 15:01:30 +0100

    Applied changes from the rework, simplified a statement

--- UntitledGame/Game/Components/EnemyNode.swift
+++ UntitledGame/Game/Components/EnemyNode.swift
@@ -12,8 +12,8 @@ class EnemyNode: SKSpriteNode {
     var type: EnemyType
     var health: Int
     
-    init(type: EnemyType,  startPosition: CGPoint, offset: CGFloat) {
-       
+    init(type: EnemyType,  startPosition: CGPoint) {
+        
         self.type = type
         self.health = type.health
         let texture = SKTexture(imageNamed: "\(type.name)/Walk/1")
@@ -23,19 +23,18 @@ class EnemyNode: SKSpriteNode {
         
         physicsBody = SKPhysicsBody(polygonFrom: CGPath(ellipseIn: CGRectMake(self.position.x-10, self.position.y-10, texture.size().width, texture.size().height), transform: nil))
         
-//        (polygonFrom: CGPath(ellipseIn: CGRectMake(self.position.x, self.position.y, texture.size().width, texture.size().height), transform: nil))
+        //        (polygonFrom: CGPath(ellipseIn: CGRectMake(self.position.x, self.position.y, texture.size().width, texture.size().height), transform: nil))
         
-//        attackCircle = SKShapeNode(ellipseOfSize: CGSize(width: 1000, height: 400))
-//        attackCircle.physicsBody = SKPhysicsBody(polygonFromPath: CGPathCreateWithEllipseInRect(CGRectMake(-500, -200, 1000, 400), nil))
+        //        attackCircle = SKShapeNode(ellipseOfSize: CGSize(width: 1000, height: 400))
+        //        attackCircle.physicsBody = SKPhysicsBody(polygonFromPath: CGPathCreateWithEllipseInRect(CGRectMake(-500, -200, 1000, 400), nil))
         
         physicsBody?.categoryBitMask = CollisionType.enemy
         physicsBody?.collisionBitMask = CollisionType.player | CollisionType.enemy
         physicsBody?.contactTestBitMask = CollisionType.player
         
-        
         physicsBody?.isDynamic = true
         physicsBody?.allowsRotation = false
-        position = CGPoint (x: startPosition.x + offset,  y: startPosition.y + offset)
+        position = startPosition
         
     }
     
@@ -50,15 +49,15 @@ class EnemyNode: SKSpriteNode {
         
         let action = SKAction.move(to: player.position, duration: distance/speed)
         run(action)
-//        let path = UIBezierPath()
-//        path.move(to: .zero)
-//        
-//        path.addLine(to: CGPoint(x: player.position.x - self.position.x, y: player.position.y - self.position.y))
-//        
-//        let movement = SKAction.follow(path.cgPath,asOffset: true, orientToPath: true, speed: type.speed)
-//        let sequence = SKAction.sequence([movement])
-//        
-//        run(sequence)
+        //        let path = UIBezierPath()
+        //        path.move(to: .zero)
+        //
+        //        path.addLine(to: CGPoint(x: player.position.x - self.position.x, y: player.position.y - self.position.y))
+        //
+        //        let movement = SKAction.follow(path.cgPath,asOffset: true, orientToPath: true, speed: type.speed)
+        //        let sequence = SKAction.sequence([movement])
+        //
+        //        run(sequence)
     }
 }
 
