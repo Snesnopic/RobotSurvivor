e83a07e08da491ac670242b59065eb74bbb49bee
Whisper010 <zinatullinlinar2003@gmail.com>
2023-12-12 11:22:31 +0100

    h

---
+++ UntitledGame/SupportingFiles/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  UntitledGame
+//
+//  Created by Linar Zinatullin on 12/12/23.
+//
+
+import Foundation
+
+enum GameState {
+    case mainScreen
+    case playing
+    case gameOver
+}
