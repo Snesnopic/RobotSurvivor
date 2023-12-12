d7e67c6157dfa1d7ba4080992b9ebfd0452a2b85
ClaudioCantCode <antonioclaudiopepe@gmail.com>
2023-12-12 10:33:32 +0100

    fixed error

--- UntitledGame/Views/GameView.swift
+++ UntitledGame/Views/GameView.swift
@@ -22,8 +22,8 @@ struct GameViewUI: UIViewRepresentable {
         let scene = GameScene()
         scene.size = CGSize(width: screenWidth, height: screenHeight)
         scene.scaleMode = .fill
-        let joystickScene = Joystick(player: scene.player)
-        joystickScene.size = CGSize(width: screenWidth, height: screenHeight)
+        
+        
         view.presentScene(scene)
         
        
