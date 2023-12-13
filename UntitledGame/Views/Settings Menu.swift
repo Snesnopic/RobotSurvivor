626dae92221437b60b4316fbe50a0c2225a0170e
Maya Navarrete Moncada <maya.navarrete10@gmail.com>
2023-12-13 12:24:37 +0100

    Settings Menu

--- UntitledGame/Views/Settings Menu.swift
+++ UntitledGame/Views/Settings Menu.swift
@@ -5,11 +5,16 @@
 //  Created by Maya Navarrete Moncada on 13/12/23.
 //
 
+
 import SwiftUI
 
 struct Settings_Menu: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Settings")
+        Text("Volume")
+        Text("Sound Effects")
+        Text("Volume")
+        Text("Credits")
     }
 }
 
