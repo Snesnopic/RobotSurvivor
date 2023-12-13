572b8237d7e681b9b08b762413a5c742ef628237
Maya Navarrete Moncada <maya.navarrete10@gmail.com>
2023-12-13 12:24:02 +0100

    Added characters in assets and a settings menu

---
+++ UntitledGame/Views/Settings Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Settings Menu.swift
+//  UntitledGame
+//
+//  Created by Maya Navarrete Moncada on 13/12/23.
+//
+
+import SwiftUI
+
+struct Settings_Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings_Menu()
+}
