9f263ea93885d0f3c18f00406d7957b09b97b1d0
Snesnopic <snesnopic@gmail.com>
2023-12-13 16:36:06 +0100

    Code for using multiple OSTs

--- UntitledGame/Views/ParentView.swift
+++ UntitledGame/Views/ParentView.swift
@@ -6,20 +6,52 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ParentView: View {
+    class AudioPlayer {
+        static var shared: AVAudioPlayer = AVAudioPlayer()
+        static var shared2: AVAudioPlayer = AVAudioPlayer()
+    }
+    
     
     @State var currentGameState: GameState = .mainScreen
     
     @StateObject var gameLogic: GameLogic = GameLogic()
     
+    var audioPlayer: AudioPlayer =  AudioPlayer()
+
     var body: some View {
         
         switch currentGameState {
         case .mainScreen: 
-            MainMenuView(currentGameState: $currentGameState)
+            MainMenuView(currentGameState: $currentGameState).onAppear(perform: {
+                do {
+                    let path = Bundle.main.url(forResource: "mainmenu", withExtension: "mp3")
+                    ParentView.AudioPlayer.shared = try AVAudioPlayer(contentsOf: path!)
+                    ParentView.AudioPlayer.shared.play()
+                }
+                catch {
+                    
+                }
+            }).onDisappear(perform: {
+                ParentView.AudioPlayer.shared.stop()
+            })
         case .playing:
-            GameView()
+            
+            GameView().onAppear(perform: {
+                do {
+                    let path = Bundle.main.url(forResource: "game1", withExtension: "mp3")
+                    ParentView.AudioPlayer.shared2 = try AVAudioPlayer(contentsOf: path!)
+                    ParentView.AudioPlayer.shared2.play()
+                }
+                catch {
+                    
+                }
+            })
+            .onDisappear(perform: {
+                ParentView.AudioPlayer.shared2.stop()
+            })
                 .environmentObject(gameLogic)
         case .gameOver:
             GameOverView()
