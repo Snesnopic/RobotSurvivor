16e06b7b3cf0938ab0bbd2cad9f0252c46f882f6
Maya Navarrete Moncada <maya.navarrete10@gmail.com>
2023-12-14 17:26:09 +0100

    Pixel art button view

--- UntitledGame/Views/NewMainMenuView.swift
+++ UntitledGame/Views/NewMainMenuView.swift
@@ -9,9 +9,64 @@ import SwiftUI
 
 struct NewMainMenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.deadBlue
+                .ignoresSafeArea()
+            
+            Image("chip3")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .offset(x: -34)
+                .opacity(0.6)
+                
+            
+            
+            VStack{
+                
+                Spacer()
+                
+                ZStack{
+                    Image("cpuHor")
+                        .interpolation(.none)
+                        .resizable()
+                        .frame(width: 315,height: 230)
+                        .padding(.bottom, 25)
+                        .shadow(radius: 15)
+                    
+                    VStack{
+                        Text("Robot")
+                            .font(.custom("Silkscreen-Bold", size: 50))
+                            .foregroundStyle(.white)
+                        Text("Survivor")
+                            .font(.custom("Silkscreen-Bold", size: 30))
+                            .foregroundStyle(.white)
+                            .padding(.top, -45)
+                            .padding(.bottom, 30)
+                    }
+                    
+                }
+                
+                
+                PixelArtButtonView(buttonImage: "ButtonPlay1", pressedImage: "ButtonPlay2",buttonPressedAction: {
+                    //TODO: add navigation to game
+                }, textView: Text("Play") .font(.custom("Silkscreen-Regular", size: 50)), textColor: .white)
+                .frame(width: 224, height:96)
+                .padding(.bottom)
+                .shadow(radius: 15)
+                
+                
+                PixelArtButtonView(buttonImage: "ButtonSett1", pressedImage: "ButtonSett2", buttonPressedAction: {
+                    //TODO: add navigation to settings
+                }, textView: Text("Settings").font(.custom("Silkscreen-Regular", size: 25)), textColor: .white)                .frame(width: 224, height:64)
+                
+                Spacer()
+                
+            }
+        }
     }
 }
+
 
 #Preview {
     NewMainMenuView()
--- UntitledGame/Views/PixelArtButtonView.swift
+++ UntitledGame/Views/PixelArtButtonView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct PixelArtButtonView: View {
+    var buttonImage: String
+    var pressedImage: String
+    var buttonPressedAction: (() -> Void)?
+    @State private var isPressed: Bool = false
+    var textView: Text?
+    var textColor: Color?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(isPressed ? pressedImage : buttonImage)
+                .interpolation(.none)
+                .resizable()
+            if let textView {
+                textView                    
+                    .offset(y: isPressed ? -2 : -7)
+                    .opacity(isPressed ? 0.5 : 1)
+                    
+            }
+        }
+        .foregroundStyle(textColor ?? .white).gesture(DragGesture(minimumDistance: 0).onChanged { _ in
+            isPressed = true
+        }.onEnded{ _ in
+            isPressed = false
+        }
+        )
     }
 }
 
 #Preview {
-    PixelArtButtonView()
+    PixelArtButtonView(buttonImage: "ButtonPlay1", pressedImage: "ButtonPlay2")
 }
