339127610dc1ddb1e0f248782e2c3b8732aca8ac
Giuseppe Casillo <g.casillox2@gmail.com>
2023-12-15 00:12:36 +0100

    added extension for TimeInterval

--- UntitledGame/Game/Components/TimeIntervalExtension.swift
+++ UntitledGame/Game/Components/TimeIntervalExtension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension TimeInterval {
+    var hourMinuteSecond: String {
+        String(format:"%d:%02d:%02d", hour, minute, second)
+    }
+    var minuteSecond: String {
+        String(format:"%d:%02d", minute, second)
+    }
+    var hour: Int {
+        Int((self/3600).truncatingRemainder(dividingBy: 3600))
+    }
+    var minute: Int {
+        Int((self/60).truncatingRemainder(dividingBy: 60))
+    }
+    var second: Int {
+        Int(truncatingRemainder(dividingBy: 60))
+    }
+}
