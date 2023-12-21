590e96e754f3bda7d8f71000562602d5e885ff8d
Giuseppe Casillo <g.casillox2@gmail.com>
2023-12-21 21:38:33 +0100

    Balanced a bit. Now the game is more challenging

--- UntitledGame/ViewModel/EnemyTypesVM.swift
+++ UntitledGame/ViewModel/EnemyTypesVM.swift
@@ -9,10 +9,10 @@ import Foundation
 
 class EnemyTypesVM {
     var enemyTypes: [EnemyType] = [
-        EnemyType(name: "Wasp", health: 15, speed: 65, points: 30, damage: 3),
-        EnemyType(name: "Scarab", health: 20, speed: 35, points: 20, damage: 7),
-        EnemyType(name: "Spider", health: 8, speed: 40, points: 15, damage: 5),
-        EnemyType(name: "Hornet", health: 10, speed: 70, points: 30, damage: 2)
+        EnemyType(name: "Wasp", health: 8, speed: 50, points: 10, damage: 2),
+        EnemyType(name: "Scarab", health: 15, speed: 35, points: 20, damage: 5),
+        EnemyType(name: "Spider", health: 20, speed: 40, points: 25, damage: 7),
+        EnemyType(name: "Hornet", health: 7, speed: 55, points: 20, damage: 3)
     ]
     static var enemyBoss: EnemyBossType =
     EnemyBossType(name: "Centipede", health: 100, speed: 70, points: 150, parts: EnemyType(name: "CentipedeBody", health: 20, speed: 70, points: 30, damage: 30))
