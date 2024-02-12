a9236455e4eb082c4d5a12138acd2d963d065acc
Snesnopic <snesnopic@gmail.com>
2024-02-12 15:08:06 +0100

    New scene used for previewing skins

--- RobotSurvivor/Game/SkinPreviewScene.swift
+++ RobotSurvivor/Game/SkinPreviewScene.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import SpriteKit
+
+class SkinPreviewScene: SKScene {
+    var skin: String = "AntiTank"
+    var isActive: Bool = true
+    init(skin: String,isActive: Bool){
+        super.init(size: CGSize(width: 50, height: 50))
+        self.skin = skin
+        self.isActive = isActive
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMove(to view: SKView) {
+        print("You are previewing \(skin)!")
+        let player:SKSpriteNode = SKSpriteNode(imageNamed: "\(skin)/Idle/1")
+        
+        let playerIdleAtlas: SKTextureAtlas = SKTextureAtlas(named: "\(skin)/Idle")
+        var playerIdleTextures: [SKTexture] = []
+        playerIdleAtlas.textureNames.sorted().forEach { string in
+            let texture = playerIdleAtlas.textureNamed(string)
+            texture.filteringMode = .nearest
+            playerIdleTextures.append(texture)
+        }
+            let playerIdleAnimation = SKAction.animate(with: playerIdleTextures, timePerFrame: 0.3)
+       
+        backgroundColor = .clear
+        player.name = "player"
+        if isActive {
+            player.run(SKAction.repeatForever(playerIdleAnimation))
+        }
+        player.size = CGSize(width: 30, height: 30)
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height:  20))
+        player.zPosition = 3
+        player.position = CGPoint(x: 25, y: 25)
+        player.physicsBody?.isDynamic = false
+        
+        
+        addChild(player)
+    }
+}
