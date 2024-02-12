7aee8b7f1aa8fc8e56ed14cc8811bb5d865f01f0
Snesnopic <snesnopic@gmail.com>
2024-02-12 16:16:23 +0100

    Fully working character selector

--- RobotSurvivor/Views/ChooseCharView.swift
+++ RobotSurvivor/Views/ChooseCharView.swift
@@ -10,13 +10,21 @@ import _SpriteKit_SwiftUI
 
 struct ChooseCharView: View {
     @Binding var selectedChar:String
+    @Binding var currentGameState: GameState
     var body: some View {
         ZStack {
+            Color.deadBlue.ignoresSafeArea()
+            
+            Image("chip3")
+                .interpolation(.none)
+                .scaledToFill()
+                .ignoresSafeArea()
+                .opacity(0.6)
             Image("cpuPower")
                 .interpolation(.none)
                 .resizable()
-                .offset(y:25)
                 .frame(width: 370,height: 400)
+                .padding(.bottom,50)
             
             ScrollView(.horizontal,showsIndicators: false) {
                 LazyHStack(alignment: .center)
@@ -26,9 +34,13 @@ struct ChooseCharView: View {
                         VStack {
                             Button(action: {
                                 selectedChar = skin
-                                print("New value: \(selectedChar)")
                             }, label: {
-                                SpriteView(scene: SkinPreviewScene(skin: skin,isActive: skin == selectedChar),options: [.allowsTransparency])
+                                if(skin == selectedChar){
+                                    SpriteView(scene: SkinPreviewScene(skin: skin,isActive: true),options: [.allowsTransparency])
+                                }
+                                else{
+                                    SpriteView(scene: SkinPreviewScene(skin: skin,isActive: false),options: [.allowsTransparency])
+                                }
                             })
                             .frame(width: 100,height: 100)
                             Text(skin)
@@ -38,18 +50,23 @@ struct ChooseCharView: View {
                     }
                 }
             }
-            .padding(.top, 120)
-            .padding(.horizontal, 50)
+            .padding(.top, 50)
+            .padding(.horizontal, 455)
             
             Text("Choose character")
                 .tracking(3.0)
                 .font(.custom("Silkscreen-Bold", size: 19.0))
                 .foregroundStyle(.white)
-            
+                .padding(.bottom,110)
+            PixelArtButtonView(buttonImage: "ButtonPlay1", pressedImage: "ButtonPlay2",buttonPressedAction: {
+                withAnimation{currentGameState = .playing}
+            }, textView: Text("Play") .font(.custom("Silkscreen-Regular", size: 50)), textColor: .white)
+            .frame(width: 224, height:96)
+            .padding(.top, 500)
         }
     }
 }
 
 #Preview {
-    ChooseCharView(selectedChar: .constant("AntiTank"))
+    ChooseCharView(selectedChar: .constant("AntiTank"), currentGameState: .constant(.chooseChar))
 }
