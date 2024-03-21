fbbdadd83a2dae92bcb0e06c225cdeea0cf9b143
Snesnopic <snesnopic@gmail.com>
2024-03-21 16:50:29 +0100

    Different stats

--- RobotSurvivor/ViewModel/EnemyTypesVM.swift
+++ RobotSurvivor/ViewModel/EnemyTypesVM.swift
@@ -13,7 +13,7 @@ class EnemyTypesVM {
         EnemyType(name: "Scarab", health: 6, speed: 35, points: 20, damage: 5),
         EnemyType(name: "Spider", health: 10, speed: 40, points: 25, damage: 7),
         EnemyType(name: "Hornet", health: 4, speed: 55, points: 20, damage: 3),
-        EnemyType(name: "CentipedeHead", health: 50, speed: 70, points: 100, damage: 12),
-        EnemyType(name: "CentipedeBody", health: 11, speed: 70, points: 12, damage: 2)
+        EnemyType(name: "CentipedeHead", health: 100, speed: 65, points: 100, damage: 12),
+        EnemyType(name: "CentipedeBody", health: 11, speed: 65, points: 12, damage: 2)
     ]
 }
