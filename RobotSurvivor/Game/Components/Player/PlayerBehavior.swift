0412e61fc3e2342bdacc45e6e4c2ee3ee2c5a7c7
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-03-24 18:24:34 +0100

    Quick fix of code i made earlier, plus it's better we start introducing comments because if someone wants to understand something, they need first ot understand what the heck is happening

--- RobotSurvivor/Game/Components/Player/PlayerBehavior.swift
+++ RobotSurvivor/Game/Components/Player/PlayerBehavior.swift
@@ -11,6 +11,8 @@ import AVFAudio
 
 extension GameScene{
     
+    //leveling up
+    
     func levelUp(){
         player.level += 1
         player.xp = 0
@@ -19,11 +21,14 @@ extension GameScene{
         gameLogic.playerLevel += 1
         gameLogic.showPowerUp = true
     }
+    
+    //whenever the +hp power up is called, this updates the maximum hp, although visually the bar will always appear kind of buggy, in fact, if you pick up +hp as first powerup, you will see the bar 1/4 empty
     public func updateHpBar() {
         let healthBarFill = healthBar.children.last!
         healthBarFill.xScale = CGFloat(Double(player.hp) / Double(player.maxHp))
     }
     
+    //basically to make this game lighter, it's important to reuse assets since the bullets and the sounds get called a lot in the update function, therefore i created an array of both bullets and sounds in which the game extracts the nodes.
     func setupBulletPool(quantityOfBullets: Int) {
         for _ in 0..<quantityOfBullets {
             let bullet = SKSpriteNode(imageNamed: "bullet")
@@ -33,17 +38,20 @@ extension GameScene{
         }
     }
     
+    //this is the extraction part of the bullets
     func getBulletFromPool() -> SKSpriteNode? {
         if bulletPool.isEmpty {
-            setupBulletPool(quantityOfBullets: 50) // Replenish the pool if empty
+            setupBulletPool(quantityOfBullets: 10)
         }
         return bulletPool.popLast()
     }
     
+    //and after the lifecycle of the bullet ended, it gets back into the array
     func returnBulletToPool(_ bullets: [SKSpriteNode]) {
         bulletPool.append(contentsOf: bullets)
     }
     
+    //same for sounds
     func setupBulletSoundPool(quantityOfSounds: Int) {
         for _ in 0..<quantityOfSounds {
             if let soundURL = Bundle.main.url(forResource: "BULLETS", withExtension: "mp3") {
@@ -58,6 +66,7 @@ extension GameScene{
         }
     }
     
+    //this is basically the play sound
     func playBulletSound() {
         guard let soundPlayer = getAvailableSoundPlayer() else {
             print("No available sound player in the pool")
@@ -68,6 +77,7 @@ extension GameScene{
         soundPlayer.play()
     }
 
+    //now this is a check for if we find the sound, then if a sound is found, there is the if statement that checks has necessary sounds, and if this happens it loads whatever is necesseray (firerate + 1), anyway this is to dynamically arrange firerate going higher if the player ever reaches that stage
     func getAvailableSoundPlayer() -> AVAudioPlayer? {
 
         for sound in bulletSoundPool {
@@ -91,9 +101,8 @@ extension GameScene{
         }
         return nil
     }
-
-
     
+    //for the same purpose of the sounds, the hitting sounds and others may be used with the same logic
     func setupShortSoundPool(name soundName: String, quantityOfSounds: Int) {
         
         let soundURL = Bundle.main.url(forResource: soundName, withExtension: "mp3")
@@ -110,6 +119,7 @@ extension GameScene{
         }
     }
     
+    //this plays the getting hit sound
     func playGettingHitSound(name: String) {
         
         guard let soundPlayer = soundPool.first else { return }
@@ -119,6 +129,7 @@ extension GameScene{
         soundPool.append(soundPlayer)
     }
     
+    //these are for configuration of bullet node
     private func configureBullet(_ bullet: SKSpriteNode) {
         bullet.texture?.filteringMode = .nearest
         bullet.name = "bullet"
@@ -134,11 +145,13 @@ extension GameScene{
         bullet.physicsBody?.isDynamic = false
     }
     
+    //this should be explained by casillo
     func findClosestEnemy() -> EnemyNode? {
         let activeEnemies = children.compactMap { $0 as? EnemyNode }
         return activeEnemies.min(by: { distanceBetween(node1: player, node2: $0) < distanceBetween(node1: player, node2: $1) })
     }
     
+    //this uses everything in this code
     func shoot() {
         var usedBullets: [SKSpriteNode] = []
         guard !isGameOver else { return }
@@ -150,7 +163,7 @@ extension GameScene{
             addChild(shot)
             
             if let closestEnemy = findClosestEnemy() {
-                let time = distanceBetween(node1: player, node2: closestEnemy) / max(Float(spd * 10), 400)
+                let time = distanceBetween(node1: player, node2: closestEnemy) / min(Float(spd * 10), 400)
                 let movement = SKAction.move(to: closestEnemy.position, duration: TimeInterval(time))
                 
                 playBulletSound()
@@ -168,6 +181,7 @@ extension GameScene{
         }
     }
     
+    //play death sound as explained above
     func playDeathSound(audioFileName: String){
         let soundNode = SKAudioNode(fileNamed: audioFileName)
         soundNode.autoplayLooped = false
