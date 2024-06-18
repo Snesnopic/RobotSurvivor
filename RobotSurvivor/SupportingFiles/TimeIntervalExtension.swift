21ec1d5dd966afc19abde7be7f5930c2b1a8f003
Snesnopic <snesnopic@gmail.com>
2024-06-18 08:57:55 +0200

    Speed boost in release mode

--- RobotSurvivor/SupportingFiles/TimeIntervalExtension.swift
+++ RobotSurvivor/SupportingFiles/TimeIntervalExtension.swift
@@ -24,3 +24,17 @@ extension TimeInterval {
         Int(truncatingRemainder(dividingBy: 60))
     }
 }
+
+public func print(_ object: Any...) {
+#if DEBUG
+    for item in object {
+        Swift.print(item)
+    }
+#endif
+}
+
+public func print(_ object: Any) {
+#if DEBUG
+    Swift.print(object)
+#endif
+}
