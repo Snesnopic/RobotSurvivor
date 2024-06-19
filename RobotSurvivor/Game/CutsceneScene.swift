76581a2d8083cb6e13690653c2024332679a472e
Snesnopic <snesnopic@gmail.com>
2024-06-19 12:33:17 +0200

    New cutscene!

--- RobotSurvivor/Game/CutsceneScene.swift
+++ RobotSurvivor/Game/CutsceneScene.swift
@@ -29,18 +29,42 @@ class CutsceneScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = .zero
         let factoryNode = SKSpriteNode(texture: factoryTexture)
         self.addChild(factoryNode)
-        let action = SKAction.move(by: CGVector(dx: 300, dy: -200), duration: 1.5)
+        let action = SKAction.move(by: CGVector(dx: 300, dy: -200), duration: 3.0)
         let sceneCamera = SKCameraNode()
         camera = sceneCamera
         self.addChild(camera!)
         camera!.run(action)
-
+        print(scene?.size)
+        camera?.setScale( 393 / (scene?.size.width)! )
         let playerIdleAtlas: SKTextureAtlas = SKTextureAtlas(named: "AntiTank/Idle")
         var playerIdleTextures: [SKTexture] = []
         playerIdleAtlas.textureNames.sorted().forEach { string in
             let texture = playerIdleAtlas.textureNamed(string)
             texture.filteringMode = .nearest
             playerIdleTextures.append(texture)
+        }
+        for i in 0..<320 {
+            let expType = Int.random(in: 0...1)
+            let explosionAtlas = SKTextureAtlas(named: "Explosions/\(expType == 1 ? "Big" : "Small")")
+            var explosionTextures: [SKTexture] = []
+            explosionAtlas.textureNames.sorted().forEach { string in
+                let texture = explosionAtlas.textureNamed(string)
+                texture.filteringMode = .nearest
+                explosionTextures.append(texture)
+            }
+            var explosionAnimation = SKAction.animate(with: explosionTextures, timePerFrame: 0.07)
+            let explosion: SKNode = SKSpriteNode(texture: nil, size: explosionTextures.first!.size())
+            explosion.xScale = 5
+            explosion.yScale = 5
+            explosion.position.y += CGFloat(Double.random(in: -150...200))
+            explosion.position.x += CGFloat(Double.random(in: -20...300))
+            explosion.zPosition = 2
+            scene!.addChild(explosion)
+            let actionSequence = SKAction.sequence([
+                SKAction.wait(forDuration: Double.random(in: (0.0)...(10.0))),
+                explosionAnimation,
+                SKAction.removeFromParent()])
+            explosion.run(actionSequence)
         }
 
         let playerWalkAtlas: SKTextureAtlas = SKTextureAtlas(named: "AntiTank/Walk")
@@ -59,10 +83,13 @@ class CutsceneScene: SKScene, SKPhysicsContactDelegate {
         player.zPosition = 10
         self.addChild(player)
 
-        let waitAction = SKAction.wait(forDuration: 2.0)
-        let moveAction = SKAction.move(by: CGVector(dx: 2000, dy: 0), duration: 5)
+        let waitAction = SKAction.wait(forDuration: 3.5)
+        let moveAction = SKAction.move(by: CGVector(dx: 2000, dy: 0), duration: 4)
+        let changeStageAction = SKAction.run {
+            GameLogic.shared.stage = .main
+        }
 
-        player.run(SKAction.sequence([waitAction, playerWalkAnimation, moveAction]))
+        player.run(SKAction.sequence([waitAction, playerWalkAnimation, moveAction, changeStageAction]))
     }
 
     override func update(_ currentTime: TimeInterval) {
